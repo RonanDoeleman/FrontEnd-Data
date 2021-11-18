8d3fea068a628ad34f34cbcae7579204e6123ee6
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2021-11-18 16:47:39 +0100

    Name change

--- indexx.js
+++ indexx.js
@@ -6,7 +6,7 @@ const fetchPokemon = () => {
 	// creeer een nieuwe array die ervoor zorgt dat alle items in de array parralel geladen worden,
 	// zo hoeft niet elk individueel item achter elkaar geladen worden maar gebeurd dit tegelijk.
 
-	for (let i = 1; i <= 15; i++) {
+	for (let i = 1; i <= 8; i++) {
 			const url = 'https://pokeapi.co/api/v2/pokemon/' + i;
 			arrayOfPromises.push(fetch(url)
 			  .then((response) => response.json())
@@ -41,130 +41,128 @@ const fetchPokemon = () => {
 
 const getPokemonData = (pokemons) => {
 
-	const margin = {top: 70, bottom: 10, left: 150, right: 0};
-    const width = 1000 - (margin.left - margin.right);
-    const height = 650 - (margin.top - margin.bottom);
+	// Geef de afmetingen van de svg aan
+	const width = 1000;
+	const height = 500;
+	const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-    // Maak een svg element aan
-    const svg = d3.select('#wrapper')
-        .append('svg')
-        .data(pokemons.sort((a,b) => d3.descending(a.weight, b.weight))) 
-        .attr('width', width + (margin.left + margin.right))
-        .attr('height', height + (margin.top + margin.bottom));
+	// Voeg de svg toe aan de id in de html
+	const svg = d3.select("#wrapper")
+			.append("svg")
+			.attr("height", height - margin.top - margin.bottom)
+			.attr("width", width - margin.left - margin.right)
+			.attr("viewBox", [0, 0, width, height]);
 
-    // Groupeer om margin toe te voegen
-    const g = svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
+	// Maak een scale aan voor de x-as
+	const x = d3.scaleBand()
+		.domain(d3.range(pokemons.length))
+		.range([margin.left, width - margin.right])
+		.padding(0.1);
 
-    // Definieer de x en y scales
-    const xscale = d3.scaleLinear()
-    .range([width - margin.left, margin.right])
+	// // Maak een scale aan voor de y-as
+	const y = d3.scaleLinear()
+		.domain([0, 1000])
+		.range([height - margin.bottom, margin.top]);
 
-    const yscale = d3.scaleBand()
-    .domain(d3.range(pokemons.length))
-	.range([margin.bottom, height - margin.top])
-    .paddingInner(0.2);
+	// Dit gebruik je om de bars te maken, ze zijn gesorteerd van hoog naar laag.
+	// Met de attributes bepaal je de grootte en breedte van elke bars
+	svg
+		.append("g")
+		.selectAll("rect")
+		.data(pokemons.sort((a,b) => d3.descending(a.weight, b.weight))) 
+		.join("rect")
+				.attr("x", (d, i) => x(i))
+				.attr("y", (d) => y(d.weight))
+				.attr("height", d => y(0) - y(d.weight))
+				.attr("width", x.bandwidth())
 
-    // Maak de axis en voeg chart labels toe
-    const xaxis = d3.axisTop().scale(xscale);
-    const g_xaxis = g.append('g').attr('class','axis')
-    g.append('g')
-      .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('font-size', '1em')
-        .attr('dx', '-13em')
-        .attr('dy', '-7em')
-        .attr('text-anchor', 'end')
-        .text('Pokemon');
-        
+	// Positioneer de x-as aan de onderkant van de chart
+	const xAxis = (g) => {
+		g.attr('transform', `translate(0, ${height - margin.bottom})`)
+		.call(d3.axisBottom(x).tickFormat(i => pokemons[i].name))
+		.attr("font-size", "1em")
+	  	.append("text")
+		  .attr("font-weight",function(d,i) {return i*100+100;})
+		  .attr("dy", "3em")
+		  .attr("dx", "60em")
+		  .attr("stroke", "black")
+		  .text("Pokémon");
+	}
 
-    const yaxis = d3.axisLeft().scale(yscale);
-    const g_yaxis = g.append('g').attr('class','axis')
-    g.append('g')
-      .append('text')
-        .attr('font-size', '1em')
-        .attr('dx', '5em')
-        .attr('dy', '-3em')
-        .attr('text-anchor', 'end')
-        .text('Weight (kg)')
-        
-
-    update(pokemons)
-
-    /////////////////////////
-
-    function update(new_data) {
-        
-        // verander de kleur van de bars voor de pokemons die meer dan 100 wegen
-        d3.select('#wrapper')
-            .selectAll('rect')
-            .data(pokemons)
-            .join('rect')
-            .attr('fill', 'orangered')
-            .transition()
-            .attr('width', function(pokemons){
-                return pokemons.weight
-            }) 
-
-        // update de scales
-        xscale.domain([(d3.max(new_data, (d) => d.weight)), 0]);
-        yscale.domain(new_data.map((d) => d.name));
-
-        //render de axis
-        g_xaxis.transition().call(xaxis);
-        g_yaxis.transition().call(yaxis);
-
-
-    // render de chart met de nieuwe data
-
-    // met join zorg je ervoor dat het juiste DOM element gekoppeld is aan dezelfde data
-    const rect = g.selectAll('rect')
-        .data(new_data, (d) => d.name)
-        .join(
-            
-            // enter de nieuwe elementen
-            (enter) => {
-                const rect_enter = enter.append('rect').attr('x', 0);
-                rect_enter.append('title');
-                return rect_enter;
-            },
-            
-            // update de bestaande elementen
-            (update) => update,
-            
-            // verwijder elementen die niet meer aan data gekoppeld zijn
-            (exit) => exit.remove()
-        );
-
-    // enter en update de oude en nieuwe elementen
-    rect.transition()
-        .attr('height', yscale.bandwidth())
-        .attr('width', (d) => xscale(d.weight))
-        .attr('y', (d) => yscale(d.name));
-        
-
-    rect.select('title').text((d) => d.name);
-    }
-
-
-    // koppel de checkbox
-    d3.select('#filter-fattest-pokemons').on('change', function() {
-        // deze functie gaat in werking als de checkbox wordt geselecteerd of gedeselecteerd
-        const checked = d3.select(this).property('checked');
-        if (checked === true) {
-            // als de checkbox aan staat...
-
-            // zorg dan ervoor dat de pokemons boven een gewicht van 200 worden gefilterd
-            const filtered_data = pokemons.filter((d) => d.weight > 200);
-
-            return update(filtered_data);  // update de chart met de gefilterde (nieuwe) data
-        } 
-        update(pokemons);
-        // als hij weer wordt gedeselecteerd  // wordt de chart geupdate met alle data die we hebben
-    });
- 
+	// Positioneer de y-as aan de linkerkant van de chart
+	const yAxis = (g) => {
+		g.attr('transform', `translate(${margin.left}, 0)`)
+		.call(d3.axisLeft(y).ticks(null, pokemons.format))
+		.attr("font-size", "1em")
+		.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("font-weight",function(d,i) {return i*100+100;})
+			.attr("dy", "-5em")
+			.attr("stroke", "black")
+			.text("Weight (KG)");
+	}
+	
+	// Hierdoor worden de bars getekend
+	svg.append("g").call(yAxis);     
+	svg.append("g").call(xAxis);      
 	svg.node();
+
+
+
+	// const g = svg.append('g')
+	// 	.attr("transform", `translate(${margin.left},${margin.top})`);
+
+	// const xscale = d3.scaleLinear().range([0, width]);
+	// const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.1);
+
+	// const xaxis = d3.axisTop().scale(xscale);
+	// const g_xaxis = g.append('g').attr('class','x axis');
+	// const yaxis = d3.axisLeft().scale(yscale);
+	// const g_yaxis = g.append('g').attr('class','y axis');
+
+	// function update(new_data) {
+	// 	xscale.domain([0, d3.max(new_data, (d) => d.height)]);
+	// 	yscale.domain(new_data.map((d) => d.pokemons));
+
+	// 	g_xaxis.transition().call(xaxis);
+	// 	g_yaxis.transition().call(yaxis);
+	
+	
+	// 	const rect = g.selectAll('rect').pokemons(new_data, (d) => d.pokemons).join(
+	// 		(enter) => {
+	// 			const rect_enter = enter.append('rect').attr('x', 0);
+	// 			rect_enter.append('title');
+	// 			return rect_enter;
+	// 		},
+
+	// 		(update) => update,
+
+	// 		(exit) => exit.remove
+	// 	);
+
+	// 	rect.transition()
+	// 		.attr('height', yscale.bandwidth())
+	// 		.attr('width', (d) => xscale(d.pokemons))
+	// 		.attr('y', (d) => yscale(d.weight));
+			
+	// 	rect.select('title').text((d) => d.pokemons)
+
+	// };
+
+	// d3.select("#filter-us-only").on('change', function() {
+		
+	// 	const checked = d3.select(this).property('checked');
+	// 	if (checked === true) {
+
+	// 		const filtered_data = pokemons.filter((d) => d.pokemons === "venasaur");
+
+	// 		update(filtered_data);
+	// 	} else {
+	// 		update(pokemons);
+	// 	}
+	// });
 
 }
 
 fetchPokemon();
+
